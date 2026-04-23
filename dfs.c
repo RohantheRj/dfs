e760f1c8c4b7c7728dc5b293ba65e98d0b49430c
Laaaalalala <152504111+RohantheRj@users.noreply.github.com>
2026-04-23 21:14:12 +0530

    Update dfs.c

--- dfs.c
+++ dfs.c
@@ -5,13 +5,14 @@ int i;
 visited[v]=1;
 printf("%d ",v);
 for(i=0;i<n;i++){
-if(adj[v][i]==1 && visited[i]==0){
+if(adj[v][i]==1&&visited[i]==0){
 dfs(i);
 }
 }
 }
 int main(){
 int e,i,j,u,v,start;
+printf("Enter number of vertices and edges: ");
 scanf("%d%d",&n,&e);
 for(i=0;i<n;i++){
 for(j=0;j<n;j++){
@@ -19,12 +20,15 @@ adj[i][j]=0;
 }
 visited[i]=0;
 }
+printf("Enter edges (u v):\n");
 for(i=0;i<e;i++){
 scanf("%d%d",&u,&v);
 adj[u][v]=1;
 adj[v][u]=1;
 }
+printf("Enter starting vertex: ");
 scanf("%d",&start);
+printf("DFS traversal:\n");
 dfs(start);
 return 0;
 }
